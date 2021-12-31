5ad44bb994d996e130ee736863b2712e1e4f004d
Jesse Dodde <jesse@schoolmaster.nl>
2021-12-31 18:31:08 +0100

    Fix issues from merging

---
+++ RaceResultsBlazor.App/Pages/TeamsResults.razor.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using RaceResultsBlazor.Models.ViewModels;
+
+namespace RaceResultsBlazor.App.Pages
+{
+    public partial class TeamsResults
+    {
+        private TeamResultsViewModel results;
+
+        [Parameter]
+        public string Title { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            var infoVM = await this.ResultsService.GetSeriesInfoAsync(this.Title);
+
+            if (infoVM == null || !infoVM.IsPublished)
+            {
+                this.NavManager.NavigateTo(string.Empty);
+                return;
+            }
+
+            await this.UpdateData();
+        }
+
+        private async Task UpdateData()
+        {
+            results = await ResultsService.GetTeamResultsAsync(Title);
+            this.StateHasChanged();
+        }
+    }
+}
+
